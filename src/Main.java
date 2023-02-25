d2a915eb6622650ac4c58a8ccf90ad320682e3d7
DragIv <dragunowivan192@gmail.com>
2023-02-26 01:05:09 +0300

    master2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello " + args[0]);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("hello " + args[2]);
+
+        System.out.print("x = ");
+        int x = scanner.nextInt();
+        System.out.print("y = ");
+        int y = scanner.nextInt();
+
+        System.out.println(3*Math.pow(Math.cos(x - (Math.PI/6)),2)/ 0.5 +Math.sin(y*y));
     }
 }
